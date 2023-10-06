eee949de5271323c99a11bff6d6f400a6fada90d
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-10-06 13:13:17 +0530

    ref: added repo url for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
         <footer className="bg-gray-800 text-white p-2 text-center relative bottom-0 left-0 right-0">
             <div className="flex justify-center items-center">
                 <a
-                    href="https://github.com/your-github-repo-url"
+                    href="https://github.com/ncrptd/fitness_tracker"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-blue-400 hover:text-blue-600 transition duration-300"
