f35baef9cf06558dcbbfc7cfd76d2dfee209389e
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-10-12 00:21:11 +0530

    ref

--- src/router.jsx
+++ src/router.jsx
@@ -16,7 +16,6 @@ export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={
             <RootLayout />
-
         }>
             <Route index element={
                 <RequiresAuth>
